eb6b1379de89f4a1d0edd83e72025838a7138d26
DDuarte <dnpd.dd@gmail.com>
2015-04-20 23:58:07 +0100

    Change Logger to print name of caller method automatically
    Also added attribute StringFormatMethod for R#

--- Common/Logger.cs
+++ Common/Logger.cs
@@ -5,12 +5,16 @@ namespace Common
 {
     public static class Logger
     {
-        public static void Log(string function, string format, params object[] args)
+        [StringFormatMethod("format")]
+        public static void Log(string format, params object[] args)
         {
+            var stackTrace = new StackTrace();
+            var callerMember = stackTrace.GetFrame(1).GetMethod();
+
             var dateStr = DateTime.Now.ToString("HH:mm:ss.fff");
             var formatStr = string.Format(format, args);
 
-            Trace.WriteLine(string.Format("{0} [{1}] {2}", dateStr, function, formatStr));
+            Trace.WriteLine(string.Format("{0} [{1}] {2}", dateStr, callerMember.Name, formatStr));
         }
     }
 }
