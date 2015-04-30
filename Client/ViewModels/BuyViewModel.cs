6aeb5624bb786cdd7e24313f59f57e295b5e9dfb
RubenCordeiro <ruben.cordeiro.eng@gmail.com>
2015-04-30 04:38:15 +0100

    Client: handle server disconnection for the buy view

--- Client/ViewModels/BuyViewModel.cs
+++ Client/ViewModels/BuyViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Net.Sockets;
 using System.Windows.Input;
 using Common;
 using GalaSoft.MvvmLight.Command;
@@ -74,53 +75,65 @@ namespace Client.ViewModels
 
         private async void BuyCommandExecute()
         {
-            var result = await MainWindow.Instance.ShowInputAsync("Buy Diginotes", "How many?",
-                new MetroDialogSettings { ColorScheme = MetroDialogColorScheme.Accented,
-                    AffirmativeButtonText = "Buy",
-                    DefaultText = "1" });
-
-            if (result == null) //user pressed cancel
-                return;
-
-            // PurchaseNotInProgress = false;
-            var session = App.Current.Session;
-            int buyQuantity = int.Parse(result);
-            var ret = App.Current.TheDigiMarket.CreatePurchaseOrder(session.Username, session.Password, buyQuantity);
-            // PurchaseNotInProgress = true;
-
-            if (ret.Error == DigiMarketError.NotFullfilled)
+            try
             {
-                var quotationResult = App.Current.TheDigiMarket.GetQuotation(session.Username, session.Password);
-                if (!quotationResult)
-                    return;
-
-                result = await MainWindow.Instance.ShowInputAsync("Change quotation",
-                    "Order was not fulfilled, specify new higher quotation value",
+                var result = await MainWindow.Instance.ShowInputAsync("Buy Diginotes", "How many?",
                     new MetroDialogSettings
                     {
                         ColorScheme = MetroDialogColorScheme.Accented,
-                        AffirmativeButtonText = "Change",
-                        DefaultText = quotationResult.Value.ToString(CultureInfo.InvariantCulture)
+                        AffirmativeButtonText = "Buy",
+                        DefaultText = "1"
                     });
 
-                if (result == null)
+                if (result == null) //user pressed cancel
                     return;
 
-                decimal newQuotation;
-                if (!decimal.TryParse(result, NumberStyles.Any, CultureInfo.InvariantCulture, out newQuotation)) return;
+                // PurchaseNotInProgress = false;
+                var session = App.Current.Session;
+                int buyQuantity = int.Parse(result);
+                var ret = App.Current.TheDigiMarket.CreatePurchaseOrder(session.Username, session.Password, buyQuantity);
+                // PurchaseNotInProgress = true;
 
-                if (App.Current.TheDigiMarket.ChangeQuotation(session.Username, session.Password, newQuotation, ret.Value.Id,true))
+                if (ret.Error == DigiMarketError.NotFullfilled)
                 {
-                    MainWindow.Instance.ShowNotification("Info", "Quotation successfully changed");
+                    var quotationResult = App.Current.TheDigiMarket.GetQuotation(session.Username, session.Password);
+                    if (!quotationResult)
+                        return;
+
+                    result = await MainWindow.Instance.ShowInputAsync("Change quotation",
+                        "Order was not fulfilled, specify new higher quotation value",
+                        new MetroDialogSettings
+                        {
+                            ColorScheme = MetroDialogColorScheme.Accented,
+                            AffirmativeButtonText = "Change",
+                            DefaultText = quotationResult.Value.ToString(CultureInfo.InvariantCulture)
+                        });
+
+                    if (result == null)
+                        return;
+
+                    decimal newQuotation;
+                    if (!decimal.TryParse(result, NumberStyles.Any, CultureInfo.InvariantCulture, out newQuotation))
+                        return;
+
+                    if (App.Current.TheDigiMarket.ChangeQuotation(session.Username, session.Password, newQuotation,
+                        ret.Value.Id, true))
+                    {
+                        MainWindow.Instance.ShowNotification("Info", "Quotation successfully changed");
+                    }
+                    else
+                    {
+                        MainWindow.Instance.ShowNotification("Error", "Error changing quotation");
+                    }
                 }
                 else
                 {
-                    MainWindow.Instance.ShowNotification("Error", "Error changing quotation");
+                    await MainWindow.Instance.ShowMessageAsync("Buy Diginotes", ret.Error.ToString());
                 }
             }
-            else
+            catch (SocketException e)
             {
-                await MainWindow.Instance.ShowMessageAsync("Buy Diginotes", ret.Error.ToString());
+                MainWindow.Instance.ShowNotification("Error", "Lost connection to the server, please restart the client");
             }
         }
 
@@ -128,7 +141,16 @@ namespace Client.ViewModels
         private void DeleteCommandExecute()
         {
             var session = App.Current.Session;
-            App.Current.TheDigiMarket.DeletePurchaseOrder(session.Username, session.Password, SelectedPurchaseOrder.Id);
+
+            try
+            {
+                App.Current.TheDigiMarket.DeletePurchaseOrder(session.Username, session.Password,
+                    SelectedPurchaseOrder.Id);
+            }
+            catch (SocketException e)
+            {
+                MainWindow.Instance.ShowNotification("Error", "Lost connection to the server, please restart the client");
+            }
             SelectedPurchaseOrder = null;
         }
 
@@ -137,7 +159,16 @@ namespace Client.ViewModels
         private void EditCommandExecute()
         {
             var session = App.Current.Session;
-            App.Current.TheDigiMarket.UpdatePurchaseOrder(session.Username, session.Password, SelectedPurchaseOrder.Id, SelectedPurchaseOrder.Value);
+
+            try
+            {
+                App.Current.TheDigiMarket.UpdatePurchaseOrder(session.Username, session.Password,
+                    SelectedPurchaseOrder.Id, SelectedPurchaseOrder.Value);
+            }
+            catch (SocketException e)
+            {
+                MainWindow.Instance.ShowNotification("Error", "Lost connection to the server, please restart the client");
+            }
         }
 
         public override void OnUpdate(Update update)
